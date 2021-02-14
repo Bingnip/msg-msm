d6dc27d4518ccc12acd1c1da262a5ed53b9d88e7
Clementine <406584573@qq.com>
2021-02-14 13:14:14 +0800

    安装axios、pubsub-js；命令：npm i -S ***；注意：-S代表生产环境依赖

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    devServer: {
+        port: 8888,
+        host: 'localhost',
+        https: false,
+        open: true,  //启动服务时自动打开浏览器访问
+    },
+
+    lintOnSave: false, //关闭格式检查
+    productionSourceMap: false, //打包时不会生成.map文件，加快打包是速度
+}
