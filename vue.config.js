77c4fcc02ffbc301fdd0b491f894cdee604339a2
Clementine <406584573@qq.com>
2021-02-16 10:33:50 +0800

    全局代理设置

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,16 @@ module.exports = {
         host: 'localhost',
         https: false,
         open: true,  //启动服务时自动打开浏览器访问
+        proxy: {  //开发环境代理配置
+            // key用变量用[]包裹，如'/dev-api'
+            [ process.env.VUE_APP_BASE_URL ]: {
+                target: process.env.VUE_APP_SERVICE_URL,  //目标服务器地址
+                changeOrigin: true,  //开启代理服务器
+                pathRewrite: {
+                    [ '^' + process.env.VUE_APP_SERVICE_URL ]: ''  //正则匹配，将左变为空
+                }
+            }
+        }
     },
 
     lintOnSave: false, //关闭格式检查
