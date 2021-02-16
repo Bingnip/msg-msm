d2370962ae5bbf14446b7cb2ed183fc103bbf7bb
Clementine <406584573@qq.com>
2021-02-16 10:57:41 +0800

    路由login页设置

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,19 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+// 默认情况下导入的是index.vue，因此可以直接省略
+import Login from '../views/login';
+
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
-];
-
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes
+  routes: [
+    {
+      path: '/login',
+      name: 'login',  //路由名称
+      component: Login  //组件对象
+    }
+  ]
 });
 
 export default router;
